a641f0f3c93ebb4cafb717cbfb7d23831cd3138c
Julio Caceres (Cthulhu) <jccaceres01@gmail.com>
2018-11-09 09:32:24 -0400

    commit
    * Fix Favicon image.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('scc_favicon_32x32.png') }}" />
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('/css/all.css') }}">
